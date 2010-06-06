aceaff8db0e84b0d943e20a1079ead6a28dd0beb
Jar.Patryk <Jar.Patryk@a3bd142e-38f9-3da5-1875-efea8288b106>
2010-06-06 18:08:25 +0000

    dodawanie edytora dodawania nowego punktu

--- trunk/inc/js/editor.js
+++ trunk/inc/js/editor.js
@@ -26,11 +26,23 @@ WDP.editorView = function()
 		olistOfPoints.appendTo(oContainer);
 	}
 
+	function fSerializeTimeObject(time)
+	{
+		var s = '{';
+		for (var x in time)
+		{
+			s += x + ':' +  time[x] + ',';
+		}
+		s = s.substr(0, s.length-1) + '}';
+		return (s);
+	}
+
 	function fAddItemToEditorList(item)
 	{
+		var time = fSerializeTimeObject(item.time);
 		olistOfPoints.
 			append
-			('<li><form id="edit_route_'+ iRouteId +'" point="'+iRouteIndex+'"><ul class="point-item"><li>' + 
+			('<li><form id="edit_route_'+ iRouteId +'" class="edit" point="'+iRouteIndex+'"><ul class="point-item"><li>' + 
 				fCreateInputText('altitude_' + iRouteIndex, item.altitude) + 
 				'</li><li>' + 
 				fCreateInputText('latitude_' + iRouteIndex, item.latitude) + 
@@ -39,6 +51,8 @@ WDP.editorView = function()
 				'</li><li>' +
 				'<input type="submit" name="save_' + iRouteIndex + 
 				'" value="zapisz" class="save" />' + 
+				'<input type="button" name="add_' + iRouteIndex + 
+				'" value="+" class="add-point" /><span class="time">' + time + '</span>' +
 				'</li></ul></form></li>');
 	}
 	
@@ -46,8 +60,16 @@ WDP.editorView = function()
 	{
 		return '<input type="text" class="point" size="5" name="' + name + '" id="' + name + '" title="'+ value + '" value="' + Math.decimal(value, 3) + '" />';
 	}
+	
+	function fCreateAddingEditor(id, node)
+	{
+		$('<li><form style="background : red" class="add" id="add_route_'+id+'">' +
+			'<ul class="point-item"><li><input type="text" value="nowy" title="46.029" id="altitude_5" name="altitude_5" size="5" class="point"></li><li><input type="text" value="nowy" title="41.98" id="latitude_5" name="latitude_5" size="5" class="point"></li><li><input type="text" value="nowy" title="-71.12" id="longitude_5" name="longitude_5" size="5" class="point"></li><li><input type="submit" class="save" value="zapisz" name="save_5"><input type="button" class="add-point" value="+" name="add_5"><span class="time">{time:0,minutes:0,seconds:0,hours:1,month:0,year:70,timezoneOffset:-60,day:4,date:1}</span></li></ul><hr />Tu jeszcze time</form></li>').insertAfter(node);
+	}
 
 	return {
-		create : fCreate
+		create : fCreate,
+		createAddEditor : fCreateAddingEditor
+		
 	};
 };
